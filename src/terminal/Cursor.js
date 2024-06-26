1131a994dc42793a22666036134489de8d07d0d8
Lukas Petervary <lukas.at.petervary@gmail.com>
2024-06-25 22:03:25 -0700

    fixed custom cursor bugs

--- src/terminal/Cursor.js
+++ src/terminal/Cursor.js
@@ -1,7 +1,7 @@
 export class CustomCursor {
     constructor() {
         this.cursorElement = document.getElementById('custom-cursor');
-        this.isLocked = true;
+        this.isLocked = false;
         this.position = { x: window.innerWidth / 2, y: window.innerHeight / 2 };
         this.delta = { dx: 0, dy: 0 };
 
@@ -19,36 +19,26 @@ export class CustomCursor {
 
             this.position.x += this.delta.dx;
             this.position.y += this.delta.dy;
-
-            // Ensure the position stays within the window bounds
-            this.position.x = Math.max(0, Math.min(window.innerWidth, this.position.x));
-            this.position.y = Math.max(0, Math.min(window.innerHeight, this.position.y));
-
-            this.cursorElement.style.transform = `translate(${this.position.x}px, ${this.position.y}px)`;
         } else {
-            this.cursorElement.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
+            this.position.x = e.clientX;
+            this.position.y = e.clientY;
         }
-    }
-
-    track() {
-        return {position: this.position, delta: this.delta};
+        this.cursorElement.style.transform = `translate(${this.position.x}px, ${this.position.y}px)`;
     }
 
     toggleLock() {
         this.isLocked = !this.isLocked;
-        this.isLocked ? this.lockCursor() : this.unlockCursor();
+        this.isLocked ? this.lock() : this.unlock();
         window.debugTerminal.log(`Cursor ${this.isLocked ? 'locked' : 'unlocked'}`)
     }
 
-    lockCursor() {
+    lock() {
         this.cursorElement.style.backgroundColor = 'blue';
-        this.position = { x: window.innerWidth / 2, y: window.innerHeight / 2 }; // Reset position to center
-        this.cursorElement.style.transform = `translate(${this.position.x}px, ${this.position.y}px)`;
         document.documentElement.requestPointerLock = document.documentElement.requestPointerLock || document.documentElement.mozRequestPointerLock || document.documentElement.webkitRequestPointerLock;
         document.documentElement.requestPointerLock();
     }
 
-    unlockCursor() {
+    unlock() {
         this.cursorElement.style.backgroundColor = 'red';
         document.exitPointerLock = document.exitPointerLock || document.mozExitPointerLock || document.webkitExitPointerLock;
         document.exitPointerLock();
